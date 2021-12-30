d9cd584da731f7a33933ebf231bfb0c8315a550e
lvfpppp <1054871894@qq.com>
2021-12-30 20:24:01 +0800

    test(motor): 测试电机闭环成功

--- applications/Main/main.c
+++ applications/Main/main.c
@@ -9,13 +9,13 @@ Motor_t M3508;
 
 #define CAN_RS_PIN    GET_PIN(A,15)
 #define MAX_3508_RPM    (469.0f)
-
+#define MOTOR_3508_RATIO     (3591.0f/187.0f)
 
 void Motor_Init(void)
 {
-    motor_init(&M3508,MOTOR_ID_1,(3591.0f/187.0f),ANGLE_CTRL_EXTRA,8192,180,-180);
-    pid_init(&M3508.ang,1,0,0,1000,MAX_3508_RPM,-MAX_3508_RPM);
-    pid_init(&M3508.spe,1,0,0,1000,10000,-10000);
+    motor_init(&M3508,MOTOR_ID_1,MOTOR_3508_RATIO,ANGLE_CTRL_EXTRA,8192,180,-180);
+    pid_init(&M3508.ang,30,0,0,1000,MAX_3508_RPM*MOTOR_3508_RATIO,-MAX_3508_RPM*MOTOR_3508_RATIO);
+    pid_init(&M3508.spe,10,0.1,0,1000,10000,-10000);
 
     /*等待电机第一次通信完毕*/
 	while(M3508.dji.Data_Valid == 0) 
@@ -28,7 +28,9 @@ void Motor_Cal(void)
     {
         Motor_AnglePIDCalculate(&M3508,Motor_Read_NowAngle(&M3508));
 
-        Motor_SpeedPIDCalculate(&M3508,M3508.ang.out);
+        Motor_Write_SetSpeed_ABS(&M3508,M3508.ang.out);
+		
+        Motor_SpeedPIDCalculate(&M3508,Motor_Read_NowSpeed(&M3508));
 
         motor_current_send(can1_dev,STDID_launch,Motor_Read_OutSpeed(&M3508),0,0,0);
 
